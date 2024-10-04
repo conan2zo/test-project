cfc8665cab32ee5e92fc7a581a6564e327e0eda4
DESKTOP-OSFKUPN\hi <rhksgns4@gmail.com>
2024-10-04 17:46:16 +0900

    test

--- test-project/src/main/java/com/conan2zo/project1/common/Dummy.java
+++ test-project/src/main/java/com/conan2zo/project1/common/Dummy.java
@@ -1,7 +1,10 @@
 package com.conan2zo.project1.common;
 
 
-import java.sql.Connection;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
 
 public class Dummy {
 
@@ -9,7 +12,63 @@ public class Dummy {
 
         Connection con = null;
 
-        Pr
+        Properties prop = null;
+
+        try {
+            prop.load(new FileReader("src/main/java/com/conan2zo/project1/common/connection.properties"));
+
+            String driver = prop.getProperty("driver");
+
+            String url = prop.getProperty("url");
+
+            Class.forName(driver);
+
+            con = DriverManager.getConnection(url,prop);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return con;
+    }
+
+    public static void close (Connection con) {
+
+        try {
+            if(con != null & !con.isClosed()){
+                con.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void close (Statement stmt) {
+
+        try {
+            if(stmt != null & !stmt.isClosed()){
+                stmt.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void close (ResultSet rset) {
+
+        try {
+            if(rset != null & !rset.isClosed()){
+                rset.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
