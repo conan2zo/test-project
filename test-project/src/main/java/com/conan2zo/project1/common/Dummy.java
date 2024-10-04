4aec23b3e24611ede5ed5a0c64dd45594660afe9
Jung-Eunmi <dmssl2486@gmail.com>
2024-10-04 17:45:35 +0900

    insert template

--- test-project/src/main/java/com/conan2zo/project1/common/Dummy.java
+++ test-project/src/main/java/com/conan2zo/project1/common/Dummy.java
@@ -1,7 +1,12 @@
 package com.conan2zo.project1.common;
 
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class Dummy {
 
@@ -9,7 +14,24 @@ public class Dummy {
 
         Connection con = null;
 
-        Pr
+        Properties prop = new Properties();
+
+        try {
+            prop.load(new FileReader("src/main/java/com/conan2zo/project1/file"));
+
+            String driver = prop.getProperty("driver");
+            String url = prop.getProperty("url");
+            Class.forName(driver);
+            con = DriverManager.getConnection(url,prop);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return con;
     }
 
 }
